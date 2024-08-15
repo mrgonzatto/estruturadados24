a2ccc7568fa5d4133afb7810b8bba2dbdaae3fee
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2024-08-14 22:07:24 -0300

    Primeiro projeto .net

---
+++ Atividades/Aula01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Comentário de uma linha
+
+/*
+    Comentário de 
+    Múltiplas linhas
+*/
+
+// Declarando variável
+int number;
+// Atribundo valor
+number = 10;
+
+// Declarando constante
+const int DAYS_IN_WEEK = 7;
+
+// Imprimindo String concatenada
+Console.WriteLine( $"A semana tem {DAYS_IN_WEEK} dias." );
