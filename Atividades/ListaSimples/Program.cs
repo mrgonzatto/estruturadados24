3de63d1888daff5b6c350017879a60d0b9c7444f
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2024-09-04 22:09:03 -0300

    Listas

---
+++ Atividades/ListaSimples/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+// Utilizando a Lista Simples
+ArrayList arrList = new();
+arrList.Add(5);
+arrList.Add("Mauricio");
+//É possível adicionar um conjunto
+// de valores de uma só vez
+arrList.AddRange(new int[] {1,2,3});
+// O método .Add() insere o valor
+// ao final do vetor
+arrList.Insert(1, 15);
+// Já o método insert, me permite
+// incluir o valor desejado na posição
+// especificada no primeiro parâmetro;
+
+// Lendo valores da Lista
+object primeiro = arrList[0]!;
+int quarto = (int)arrList[3]!;
+
+// percorrendo a Lista com foreach
+foreach(object obj in arrList)
+{
+    Console.Write($" | {obj}");
+}
+
+//obtendo o tamanho total da lista
+// Quantos elementos tem
+int tamanho = arrList.Count;
+// obter a capacidade
+// Quandos podem ser armazenados
+int capacidade = arrList.Capacity;
+
+// percorrendo no modo clássico
+for(int i = 0; i < arrList.Count; i++)
+{
+    Console.Write( $" | {arrList[i]}" );
+}
+
+// Recursos importantes da Lista
+// Verificar se um valor é contido na lista
+bool contemNome = arrList.Contains("Mauricio");
+if(!contemNome)
+    Console.WriteLine("Nome Mauricio não encontrado");
+
+    // Para saber o índice que contem o valor buscado
+    // Neste caso se o valor existir na lista
+    // Ele retorna o índice (número inteiro)
+    // Caso o valor não exista na lista, retorna -1
+    int indiceDoValor = 
+        arrList.IndexOf("Mauricio");
+    if(indiceDoValor >= 0)
+        Console.WriteLine(
+            $"Mauricio está em [{indiceDoValor}]"
+        );
+    else
+        Console.WriteLine("É, realmente não tem.");
+
+// É possível remover itens da lista
+arrList.Remove("Mauricio"); // Remove pelo valor
+arrList.RemoveAt(4); // Remove no índice
+arrList.RemoveRange(0,2); 
+// O primeiro parametro é o índice 
+// e o segundo é quantas casas após o índice
