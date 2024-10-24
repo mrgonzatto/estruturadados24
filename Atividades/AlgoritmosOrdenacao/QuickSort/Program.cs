c9c8004fd846ca78313997324f033a3b5885e6cc
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2024-10-23 22:04:47 -0300

    Arvores

---
+++ Atividades/AlgoritmosOrdenacao/QuickSort/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Testando o Quick Sort");
+
+int[] values = new int[] {0, 15, 24, 1, 3, 6, 12, 9};
+
+QuickSort.QuickSort.Print<int>(values);
+Console.WriteLine("Iniciando ordenação.");
+QuickSort.QuickSort.Sort<int>(values, 0, values.Length - 1 );
+Console.WriteLine("Finzando ordenação.");
+QuickSort.QuickSort.Print<int>(values);
---
+++ Atividades/ArvoresBasicas/Program.cs
@@ -0,0 +1,62 @@
+﻿using ArvoresBasicas;
+
+/*
+Tree<int> tree = new Tree<int>();
+tree.Root = new Node<int>() { Data = 100 };
+tree.Root.Children = 
+    new List<Node<int>>{
+        new Node<int>() { Data = 50, Parent = tree.Root },
+        new Node<int>() { Data = 1, Parent = tree.Root },
+        new Node<int>() { Data = 150, Parent = tree.Root },
+    };
+
+Console.WriteLine(
+    $"Nivel teste {tree.Root.Children[0].GetHeight()}"
+);
+
+Node<int> node12 = new Node<int>() { 
+    Data = 12, 
+    Parent =  tree.Root.Children[0]
+};
+Node<int> node45 = new Node<int>() { 
+    Data = 45, 
+    Parent = node12 
+};
+Node<int> node21 = new Node<int>() { 
+    Data = 21, 
+    Parent = node12 
+};
+
+node12.Children!.Add(node45!);
+node12.Children!.Add(node21!);
+
+tree.Root.Children[0].Children!.Add(node12);
+
+tree.PrintTree(tree.Root);
+
+*/
+
+Tree<Person> tree = new Tree<Person>();
+tree.Root = new Node<Person>(){
+    Data = new Person {Name = "Mauricio", Role = "CEO"},
+};
+
+tree.Root.Children = new List<Node<Person>>(){
+    new Node<Person>{ 
+        Data = new Person{
+            Name = "John Smith", 
+            Role = "Head -of Development",
+        },
+        Parent = tree.Root
+    },
+    new Node<Person>{ 
+        Data = new Person{Name = "Mary Fox", Role = "Head of Research"},
+        Parent = tree.Root
+    },
+    new Node<Person>{ 
+        Data = new Person{Name = "lily Smith", Role = "Head of Sales"},
+        Parent = tree.Root
+    }
+};
+
+
